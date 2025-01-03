7cd3567018258297e01fd8a8f7dcb3b66cb38c83
LancersBucket <andrewonline00@gmail.com>
2025-01-03 16:44:51 -0500

    init

---
+++ ToggleSilentMessages.plugin.js
@@ -0,0 +1,18 @@
+/**
+ * @name ToggleSilentMessages
+ * @displayName ToggleSilentMessages
+ * @description Adds a button to toggle sending silent messages
+ * @author LancersBucket
+ * @authorId 355477882082033664
+ * @version 1.0.0
+ * @source https://github.com/LancersBucket/ToggleSilentMessages
+ * @updateUrl https://raw.githubusercontent.com/LancersBucket/ToggleSilentMessages/refs/heads/main/ToggleSilentMessages.plugin.js
+ */
+/*@cc_on
+@if (@_jscript)
+
+var shell = WScript.CreateObject("WScript.Shell");
+shell.Popup("It looks like you've mistakenly tried to run me directly. That's not how you install plugins. \n(So don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
+
+@else@*/
+
